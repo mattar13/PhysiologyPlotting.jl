a050d29a595404abe38f5fd11167b690de4cb376
mattar13 <mtarchick13@gmail.com>
2023-04-19 17:37:36 -0400

    updated module

--- src/PhysiologyPlotting.jl
+++ src/PhysiologyPlotting.jl
@@ -1,10 +1,21 @@
 module PhysiologyPlotting
 
-# Write your package code here.
-include("DefaultSettings.jl") #This requires PyPlot
-include("PlottingUtilities.jl")
+using ElectroPhysiology
+import ElectroPhysiology: Experiment, readABF, parseABF
+
+using Plots, PyPlot
+
+# Include all the plotting utilities
+include("utilities.jl")
+
+# Include all of the PyPlot functions
 include("PhysPyPlot.jl")
 export plot_experiment
 
+#=
+# Write your package code here.
+include("DefaultSettings.jl") #This requires PyPlot
+export plot_experiment
+=#
 
 end
