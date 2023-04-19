682bf20d93dd5e9792925540579073869b8e520f
mattar13 <mtarchick13@gmail.com>
2023-04-19 17:45:20 -0400

    updated package

--- test/revision.jl
+++ test/revision.jl
@@ -15,5 +15,4 @@ truncate_data!(data, t_post = 5.0)
 #%%
 fig, ax = plt.subplots(1)
 plot_experiment(ax, data, channels = 2, color = :black, alpha = 0.2, xlims = (-0.25, 5.0))
-plot_experiment(ax, filt_data, channels = 2, color = :red, xlims = (-0.25, 5.0))
 fig
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using PhysiologyPlotting
 using Test
 
-@testset "PhysiologyPlotting.jl" begin
-    # Write your tests here.
+test_file = raw"to_analyze.abf"
+data = readABF(test_file)
+
+@testset "Testing basic quick plotting" begin
+    plot_experiment(ax, data, channels = 2, color = :black, alpha = 0.2, xlims = (-0.25, 5.0))
 end
