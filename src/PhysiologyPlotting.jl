3623db843760d16707b714d8a35357077d6ea27f
mattar13 <mtarchick13@gmail.com>
2023-04-19 16:58:00 -0400

    migrated files from PhysiologyAnalysis.jl

--- src/PhysiologyPlotting.jl
+++ src/PhysiologyPlotting.jl
@@ -1,5 +1,10 @@
 module PhysiologyPlotting
 
 # Write your package code here.
+include("DefaultSettings.jl") #This requires PyPlot
+include("PlottingUtilities.jl")
+include("PhysPyPlot.jl")
+export plot_experiment
+
 
 end
