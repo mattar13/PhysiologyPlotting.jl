394e6cd8a0b1f75e418436d11a8afb06e5c218bc
mattar13 <mtarchick13@gmail.com>
2023-04-19 21:05:06 -0400

    fixed another error in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ test_file = raw"to_analyze.abf"
 data = readABF(test_file)
 
 @testset "Testing basic quick plotting" begin
-    plot_experiment(ax, data, channels = 2, color = :black, alpha = 0.2, xlims = (-0.25, 5.0))
+    plot_experiment(data, channels = 2, color = :black, alpha = 0.2, xlims = (-0.25, 5.0))
 end
