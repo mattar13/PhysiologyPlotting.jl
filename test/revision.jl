8211c5af2e6fea8563530a6ee9c3c8524371e1e9
mattar13 <mtarchick13@gmail.com>
2023-07-31 10:33:27 -0400

    need to update ElectroPhysiology.jl to continue

--- test/revision.jl
+++ test/revision.jl
@@ -1,17 +1,27 @@
 using Revise
 using ElectroPhysiology
-using PhysiologyPlotting
 using PyPlot; PyPlot.pygui(true)
+using PhysiologyPlotting
+#%% Make a wave plot
+
 
 #%% Section 1. Revision of some plotting tools
 test_file = raw"test/to_analyze.abf"
-data = readABF(test_file)
-average_sweeps!(data)
-#filt_data = data_filter(data)
-data *= 1000.0
-baseline_adjust!(data)
-truncate_data!(data, t_post = 5.0)
+data = readABF(test_file) |> data_filter
+data_ch1 = getchannel(data, 1)
+#%%
+#We need a method to sort the files by the photon intensity without actually measuring it
+fig, axs = plt.subplots(2)
+waveplot(axs[1], data_ch1)
+plot_experiment(axs[2], data_ch1)
 
+#%%
+cvals = minimum(data, dims = 2)[:,1,1]
+cvals .-= minimum(cvals)
+cvals ./= maximum(cvals)
+fig = plot_experiment(data, xlims = (-0.1, 3.2), linewidth=3.0, ylabel = "Voltage (μV)", channels = "Vm_prime", cvals = cvals, color = "brg");
+fig.savefig(raw"C:\Users\mtarc\The University of Akron\RetinaRig - General\Presentations\logo.svg", transparent=true)
+     #channels = "Vm_prime", color = "turbo",   
 #%%
 fig, ax = plt.subplots(1)
 plot_experiment(ax, data, channels = 2, color = :black, alpha = 0.2, xlims = (-0.25, 5.0))
