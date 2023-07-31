694a160f43cde1f882739f7a0d6a5ab89a932717
mattar13 <mtarchick13@gmail.com>
2023-07-31 10:16:58 -0400

    updated files in PhysPyPlot folder

--- src/PhysPyPlot/PhysPyPlot.jl
+++ src/PhysPyPlot/PhysPyPlot.jl
@@ -74,63 +74,41 @@ function plot_experiment(axis::Vector{T}, exp::Experiment; kwargs...) where T #I
     end
 end
 
-function plot_experiment(exp::Experiment; layout = nothing, channels = nothing, kwargs...)
-    if !isnothing(layout)
-        plot_layout = layout
-    elseif !isnothing(channels)
-        if isa(channels, Vector{Int64}) || isa(channels, Vector{String})
-            plot_layout = (length(channels))
-        elseif isa(channels, Int64) || isa(channels, String)
-            plot_layout = 1
+function plot_experiment(exp::Experiment; layout = nothing, channels = nothing, st = :trace, kwargs...)
+    if st == :trace
+        if !isnothing(layout)
+            plot_layout = layout
+        elseif !isnothing(channels)
+            if isa(channels, Vector{Int64}) || isa(channels, Vector{String})
+                plot_layout = (length(channels))
+            elseif isa(channels, Int64) || isa(channels, String)
+                plot_layout = 1
+            end
+        else
+            plot_layout = (size(exp, 3))
         end
-    else
-        plot_layout = (size(exp, 3))
-    end
-    println(plot_layout)
-    fig, axis = plt.subplots(plot_layout)
-    if plot_layout == 1 || plot_layout == (1)
-        plot_experiment(axis, exp::Experiment; channels=1, kwargs...)
-    else
-        for (ch, axis) in enumerate(axis)
-            plot_experiment(axis, exp::Experiment; 
-                channels=ch, 
-                xlabel = ch == length(exp.chNames),
-                kwargs...
-            )
+        fig, axis = plt.subplots(plot_layout)
+        if plot_layout == 1 || plot_layout == (1)
+            plot_experiment(axis, exp::Experiment; channels=1, kwargs...)
+        else
+            for (ch, axis) in enumerate(axis)
+                plot_experiment(axis, exp::Experiment; 
+                    channels=ch, 
+                    xlabel = ch == length(exp.chNames),
+                    kwargs...
+                )
+            end
         end
+        return fig
+    elseif st == :waveplot
+        return nothing
+    elseif st == :trace3D
+        return nothing
     end
-    return fig
 end
 
-function add_scalebar(axis, loc::Tuple{T,T}, dloc::Tuple{T,T};
-    fontsize=10.0, lw=3.0,
-    xlabeldist=30.0, ylabeldist=15.0,
-    xunits="ms", yunits="μV",
-    xconvert=1000.0, yconvert=1.0, #this converts the units from x and y labels. x should be in ms
-    xround=true, yround=true,
-    kwargs...
-) where {T<:Real}
-    x, y = loc
-    dx, dy = dloc
-    x0, xmax = plt.xlim()
-    data_width = (xmax - x0)
-    y0, ymax = plt.ylim()
-    data_height = abs(ymax - y0)
+function waveplot(exp::Experiment)
 
-    #println(data_width / xlabeldist) #debug options 
-    #println(data_height / ylabeldist) #debug options
-    axis.plot([x, x + dx], [y, y], c=:black, lw=lw; kwargs...) #add a vertical line
-    axis.plot([x, x], [y, y + dy], c=:black, lw=lw; kwargs...)
-    if yround
-        yscale = round(Int64, dy * yconvert)
-    else
-        yscale = dy * yconvert
-    end
-    if xround
-        xscale = round(Int64, dx * xconvert)
-    else
-        xscale = dx * xconvert
-    end
-    axis.annotate("$yscale $yunits", (x - (data_width / xlabeldist), y + dy / 2), va="center", ha="center", rotation="vertical", fontsize=fontsize)
-    axis.annotate("$xscale $xunits", (x + dx / 2, y - (data_height / ylabeldist)), va="center", ha="center", rotation="horizontal", fontsize=fontsize)
+
+
 end
---
+++ src/PhysPyPlot/PlottingAddons.jl
@@ -0,0 +1,73 @@
+function add_scalebar(axis, loc::Tuple{T,T}, dloc::Tuple{T,T};
+    fontsize=10.0, lw=3.0,
+    xlabeldist=30.0, ylabeldist=15.0,
+    xunits="ms", yunits="μV",
+    xconvert=1000.0, yconvert=1.0, #this converts the units from x and y labels. x should be in ms
+    xround=true, yround=true,
+    kwargs...
+) where {T<:Real}
+    x, y = loc
+    dx, dy = dloc
+    x0, xmax = plt.xlim()
+    data_width = (xmax - x0)
+    y0, ymax = plt.ylim()
+    data_height = abs(ymax - y0)
+
+    #println(data_width / xlabeldist) #debug options 
+    #println(data_height / ylabeldist) #debug options
+    axis.plot([x, x + dx], [y, y], c=:black, lw=lw; kwargs...) #add a vertical line
+    axis.plot([x, x], [y, y + dy], c=:black, lw=lw; kwargs...)
+    if yround
+        yscale = round(Int64, dy * yconvert)
+    else
+        yscale = dy * yconvert
+    end
+    if xround
+        xscale = round(Int64, dx * xconvert)
+    else
+        xscale = dx * xconvert
+    end
+    axis.annotate("$yscale $yunits", (x - (data_width / xlabeldist), y + dy / 2), va="center", ha="center", rotation="vertical", fontsize=fontsize)
+    axis.annotate("$xscale $xunits", (x + dx / 2, y - (data_height / ylabeldist)), va="center", ha="center", rotation="horizontal", fontsize=fontsize)
+end
+
+function add_sig_bar(axes, x::Real, y::Real; 
+    level = "*", color = :black, 
+    pointer = false,
+    pointer_dx = 0.5,
+    pointer_ylims = [2.0, 3.0], 
+    lw = 1.0, fs = 12.0, ls = "solid"
+)    
+    #println(level)
+    if isa(level, Float64)
+        println(level)
+        println(sig_level(level))
+
+        level = sig_level(level)
+        println(level)
+    end
+    println(level)
+    if level != "n.s"
+         axes.text(x, y, level, ha = "center", va = "center", fontsize = fs, color = color)
+         if pointer
+              #draw top bar
+              xs_top = (x-pointer_dx, x+pointer_dx)
+              ys_top = (y, y)
+              axes.plot(xs_top, ys_top, color = color, lw = lw, linestyle = ls)
+
+              xs_left = (x-pointer_dx, x-pointer_dx)
+              ys_left = (y, pointer_ylims[1])
+              axes.plot(xs_left, ys_left, color = color, lw = lw, linestyle = ls)
+              
+              xs_right = (x+pointer_dx, x+pointer_dx)
+              ys_right = (y, pointer_ylims[2])
+              axes.plot(xs_right, ys_right, color = color, lw = lw, linestyle = ls)
+         end
+    end
+end
+
+function add_sig_bar(axis, xs::Vector{T}, ys::Vector{T}; kwargs...) where T <: Real
+    for i in axes(xs, 1)
+         add_sig_bar(axis, xs[i], ys[i]; kwargs...)
+    end
+end
