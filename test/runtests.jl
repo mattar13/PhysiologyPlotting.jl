253133986cf66108bd7751d88f3fe216c41bc767
mattar13 <mtarchick13@gmail.com>
2023-08-04 17:31:32 -0400

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,20 @@ using Test
 test_file = raw"to_analyze.abf"
 data = readABF(test_file)
 
-@testset "Testing basic quick plotting" begin
+@testset "Testing PyPlot plotting" begin
+    #Test the default frontend
+    using PyPlot
+    @test PhysiologyPlotting.frontend == :PyPlot
+
     plot_experiment(data, channels = 2, color = :black, alpha = 0.2, xlims = (-0.25, 5.0))
+end
+
+@testset "Testing CairoMakie" begin 
+    using CairoMakie
+    @test PhysiologyPlotting.frontend == :CairoMakie
+end
+
+@testset "Testing GLMakie" begin
+    using GLMakie
+    @test PhysiologyPlotting.frontend == :GLMakie
 end
