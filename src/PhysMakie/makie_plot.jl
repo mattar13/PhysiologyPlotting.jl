99743f186c258adac72758b8a932660021d6e3c1
mattar13 <mtarchick13@gmail.com>
2023-08-04 17:31:27 -0400

    added makie_plot and makie plotting

---
+++ src/PhysMakie/makie_plot.jl
@@ -0,0 +1,17 @@
+function plot_experiment(ax, EXP::Experiment; kwargs...)
+     line_arr = []
+     x = EXP.t
+     for (idx_ch, ch) in enumerate(eachchannel(EXP))
+          for (idx_swp, trial) in enumerate(eachtrial(ch)) 
+               y = trial.data_array[1, :, 1]
+               #println(y)
+               lin_ax = lines!(ax, x, y; kwargs...)
+               push!(lines_arr, lin_ax)
+          end
+     end
+     return line_arr
+
+     #here are some settings
+     hidespines!(ax, :t, :r)
+     hidedecorations!(ax, grid = true, ticks = false, ticklabels = false)
+end
