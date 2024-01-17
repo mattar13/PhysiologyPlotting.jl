d01023bba514755b8cc876405f5e83fcb31788c2
mattar13 <mtarchick13@gmail.com>
2024-01-17 10:12:52 -0800

    commented out build

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,4 @@
+#= We don't need to build CONDA anymore because pyplot will not be automatically installed
 import Pkg, Conda
 @info "Building PyCall for PyPlot from CONDA!"
 Conda.pip_interop(true)
@@ -6,3 +7,4 @@ Conda.add("matplotlib")
 ENV["PYTHON"] = joinpath(Conda.ROOTENV, "bin", "python")
 Pkg.build("PyCall")
 @info "PyCall successfully build"
+=#
