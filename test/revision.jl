96b697478e908261452e2d73f6ab60f0530b3be1
mattar13 <mtarchick13@gmail.com>
2024-01-17 16:19:47 -0800

    testing some revisions

--- test/revision.jl
+++ test/revision.jl
@@ -1,18 +1,35 @@
-using Pkg; Pkg.activate("test")
 using Revise
 using ElectroPhysiology
 using PhysiologyPlotting
+PhysiologyPlotting.frontend
+using GLMakie
+PhysiologyPlotting.frontend
+#using Pkg; Pkg.activate("test")
 
-#using GLMakie
+time = collect(1:0.1:100) #Create some time range
+data = rand(5, length(time), 1) #Create some dummy data
+exp = ElectroPhysiology.Experiment(data)
+
+f = Figure(size = (2.5, 2.5))
+ax1 = Axis(f[1,1], 
+     title = "Experiment Plot Test",
+     xlabel = "Time (ms)", 
+     ylabel = "Response (μV)"
+)
+plot_experiment(ax1, exp)
+
+#%%
 using PyPlot
-PyPlot.pygui(true)
+
+
+
+
 
 #%% We can  use maybe a gaussian curve to properly plot the points maximum value
 xs = collect(1:6)
 yvals = rand(100, maximum(xs))
 
 #%%
-PhysiologyPlotting.__init__()
 fig, ax = plt.subplots(3, 2)
 for i in xs
      ys = yvals[:, i]
@@ -22,9 +39,9 @@ end
 add_border(ax[1], xpad_ratio = 0.1, ypad_ratio = 0.1)
 #%%
 #%% Make and plot ERG episodic data
-
 data = readABF(raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\P14_Wildtype\2019_11_10_P14WT\Mouse2_P14_WT\BaCl_LAP4\Rods" |> parseABF)# |> data_filter
 data_filter!(data)
+
 #%% Generate a makie plot
 dpi = 600
 f = Figure(size = (2.5, 2.5))
