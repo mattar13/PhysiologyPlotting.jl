5b3ee0089e3e2d6bbad295ca1ce48e74e2d7d415
mattar13 <mtarchick13@gmail.com>
2024-02-14 11:19:54 -0800

    updated makie_plot

--- src/PhysMakie/makie_plot.jl
+++ src/PhysMakie/makie_plot.jl
@@ -1,29 +1,38 @@
 """
      plot_experiment(ax)
 """
-function plot_experiment(ax, EXP::Experiment; 
-     channels=1, sweeps = :all, 
-     yaxes=true, xaxes=true, #Change this, this is confusing
-     xlims = nothing, ylims = nothing,
-     color = :black, cvals = nothing, clims = (0.0, 1.0), #still want to figure out how this wil work
-     ylabel = nothing, xlabel = nothing,
-     linewidth = 1.0, kwargs...
-)
+function plot_experiment(ax, exp::Experiment, trial::Int64, channel::Int64; color = :black, kwargs...)
      println("Working with GLMakie")
      line_arr = []
-     x = EXP.t
-     for (idx_ch, ch) in enumerate(eachchannel(EXP))
-          for (idx_swp, trial) in enumerate(eachtrial(ch)) 
-               y = trial.data_array[1, :, 1]
-               #println(y)
-               lin_ax = lines!(ax, x, y; kwargs...)
-               push!(line_arr, lin_ax)
-          end
-     end
-     
-     #here are some settings
+     x = exp.t
+     y = exp.data_array[trial, :, channel]
+     #println(y)
+     lin_ax = lines!(ax, x, y; color = color, kwargs...)
+     push!(line_arr, lin_ax)
      hidespines!(ax, :t, :r)
-     hidedecorations!(ax, grid = true, ticks = false, ticklabels = false)
+     hidedecorations!(ax, grid = true, ticks = false, ticklabels = false, label = false)
+     return line_arr
+end
+
+function plot_experiment(ax, exp::Experiment; trials::Union{Int64, Nothing} = nothing, channels::Union{Int64, Nothing} = nothing, kwargs...)
+     n_trials, n_data, n_channels = size(exp)
+     line_arr = []
+     if isnothing(trials) && isnothing(channels) #plot all of the trials and channels
+          for t in 1:n_trials, c in 1:n_channels
+               line_ax = plot_experiment(ax, exp, t, c; kwargs...)
+          end
+     elseif isnothing(trials) && !isnothing(channels)
+          for t in 1:n_trials
+               line_ax = plot_experiment(ax, exp, t, channels; kwargs...)
+          end
+     elseif !isnothing(trials) && isnothing(channels)
+          for c in 1:n_channels
+               line_ax = plot_experiment(ax, exp, trials, c; kwargs...)
+          end
+     else
+          line_ax = plot_experiment(ax, exp, trials, channels; kwargs...)
+     end
+     push!(line_arr, line_ax...)
      return line_arr
 end
 
